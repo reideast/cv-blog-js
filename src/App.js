eefeae108c2fe04a4b907bf84165b5c76fac7b81
Andrew Reid East <andrew@andreweast.net>
2020-01-04 19:39:48 +0000

    Begin a sample walkthrough on CSS Grid
    See: https://css-tricks.com/new-year-new-job-lets-make-a-grid-powered-resume/

--- src/App.js
+++ src/App.js
@@ -1,10 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <CV/>
+                <TestApi/>
+            </div>
+            // <div className="App">
+            //     <header className="App-header">
+            //         <img src={logo} className="App-logo" alt="logo" />
+            //         <h1 className="App-title">Welcome to React</h1>
+            //     </header>
+            // </div>
+        );
+    }
+}
+
+class CV extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <main className='wrapper'>
+                <article className='cv-grid-container'>
+                    <section className='cv-grid-section-name cv-grid-section'>
+                        <hr />
+                    </section>
+                    <section className='cv-grid-section-photo cv-grid-section'>
+                        <div className='circle'></div>
+                    </section>
+                    <section className='cv-grid-section-about cv-grid-section'>
+                        <hr />
+                        <hr />
+                    </section>
+                    <section className='cv-grid-section-work cv-grid-section'>
+                        <hr />
+                        <hr />
+                        <hr />
+                        <hr />
+                        <hr />
+                        <hr />
+                    </section>
+                    <section className='cv-grid-section-education cv-grid-section'>
+                        <hr />
+                        <hr />
+                    </section>
+                    <section className='cv-grid-section-community cv-grid-section'>
+                        <hr />
+                        <hr />
+                    </section>
+                    <section className='cv-grid-section-skills cv-grid-section'>
+                        <hr />
+                        <hr />
+                    </section>
+                </article>
+            </main>
+        )
+    }
+}
+
+class TestApi extends Component {
     constructor(props) {
         super(props);
         this.state = { apiResponse: "" };
@@ -24,11 +92,7 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
+            <div>
                 <p className="App-intro">{ this.state.apiResponse }</p>
             </div>
         );
