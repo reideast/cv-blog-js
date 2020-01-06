3f1673bbd978e286254bfceb271d12f99cd502ac
Andrew Reid East <andrew@andreweast.net>
2020-01-06 21:03:10 +0000

    Extract CV sections to own components

--- src/CV.js
+++ src/CV.js
@@ -8,42 +8,82 @@ export class CV extends Component {
 
     render() {
         return (
-            <main className='wrapper'>
+            <main id='cv-wrapper'>
                 <article className='cv-grid-container'>
-                    <section className='cv-grid-section-name cv-grid-section'>
-                        <hr />
-                    </section>
-                    <section className='cv-grid-section-photo cv-grid-section'>
-                        <div className='circle'></div>
-                    </section>
-                    <section className='cv-grid-section-about cv-grid-section'>
-                        <hr />
-                        <hr />
-                    </section>
-                    <section className='cv-grid-section-work cv-grid-section'>
-                        <hr />
-                        <hr />
-                        <hr />
-                        <hr />
-                        <hr />
-                        <hr />
-                    </section>
-                    <section className='cv-grid-section-education cv-grid-section'>
-                        <hr />
-                        <hr />
-                    </section>
-                    <section className='cv-grid-section-community cv-grid-section'>
-                        <hr />
-                        <hr />
-                    </section>
-                    <section className='cv-grid-section-skills cv-grid-section'>
-                        <hr />
-                        <hr />
-                    </section>
+                    <CVName />
+                    <CVPhoto />
+                    <CVAbout />
+                    <CVWorkExperience />
+                    <CVEducation />
+                    <CVCommunity />
+                    <CVSkills />
                 </article>
             </main>
         );
     }
 }
 
-// export default CV; // TODO: Do not believe 'default' is needed, since all classes that use it can just specify { CV }
+class CVName extends Component {
+    render() {
+        return <section className='cv-grid-section-name cv-grid-section'>
+            <hr />
+        </section>
+    }
+}
+
+class CVPhoto extends Component {
+    render() {
+        return <section className='cv-grid-section-photo cv-grid-section'>
+            <div className='circle'></div>
+        </section>;
+    }
+}
+
+class CVAbout extends Component {
+    render() {
+        return <section className='cv-grid-section-about cv-grid-section'>
+            <hr />
+            <hr />
+        </section>;
+    }
+}
+
+class CVWorkExperience extends Component {
+    render() {
+        return <section className='cv-grid-section-work cv-grid-section'>
+            <hr />
+            <hr />
+            <hr />
+            <hr />
+            <hr />
+            <hr />
+        </section>;
+    }
+}
+
+class CVEducation extends Component {
+    render() {
+        return <section className='cv-grid-section-education cv-grid-section'>
+            <hr />
+            <hr />
+        </section>;
+    }
+}
+
+class CVCommunity extends Component {
+    render() {
+        return <section className='cv-grid-section-community cv-grid-section'>
+            <hr />
+            <hr />
+        </section>;
+    }
+}
+
+class CVSkills extends Component {
+    render() {
+        return <section className='cv-grid-section-skills cv-grid-section'>
+            <hr />
+            <hr />
+        </section>;
+    }
+}
