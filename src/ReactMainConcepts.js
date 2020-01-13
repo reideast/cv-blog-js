2ec3048163191c2abad8395a2653f2d268552cba
Andrew Reid East <andrew@andreweast.net>
2020-01-13 23:49:19 +0000

    Basic JSX

--- src/ReactMainConcepts.js
+++ src/ReactMainConcepts.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 // React tutorial, see: https://reactjs.org/docs/hello-world.html
 export class ReactMainConcepts extends Component {
     render() {
+        function greet(user) {
+            if (user) {
+                return <h1>Hello, { user }!</h1>;
+            } else {
+                return <h1>Hello, world!</h1>;
+            }
+        }
         return (
-            <h1>Hello, world!</h1>
+            <div>
+                { greet() }
+                { greet("Andrew") }
+            </div>
         );
     }
 }
