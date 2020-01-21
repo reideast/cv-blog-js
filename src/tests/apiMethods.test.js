9bfb0a66f4af5f3cdbb0c19e5a7e0a96619fc384
Andrew Reid East <andrew@andreweast.net>
2020-01-21 21:51:29 +0000

    Use describe() blocks to organise tests

--- src/tests/apiMethods.test.js
+++ src/tests/apiMethods.test.js
@@ -2,81 +2,86 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { fetchFromApi, generateApiLoadingOrElements } from '../apiMethods';
 
-beforeAll(() => {
-    global.fetch = jest.fn(); // So that it can be used with mockImplementation()
-});
+describe('fetchFromApi', () => {
+    beforeAll(() => {
+        global.fetch = jest.fn(); // So that it can be used with mockImplementation()
+    });
 
-it('updates state after loading is done', (informJestThatTestIsDone) => {
-    fetch.mockImplementation(() => {
-        return Promise.resolve({
-            status: 200,
-            text: () => {
-                return Promise.resolve('set');
+    it('updates state after loading is done', (informThatTestIsDone) => {
+        fetch.mockImplementation(() => {
+            return Promise.resolve({
+                status: 200,
+                text: () => {
+                    return Promise.resolve('set');
+                }
+            })
+        });
+
+        let myState = { results: 'unchanged', apiFetchCompleted: false };
+        let objThis = {
+            setState: (newState) => {
+                myState = newState;
             }
-        })
+        };
+
+        expect(myState.results).toEqual('unchanged');
+        const returnedPromise = fetchFromApi.call(objThis, 'api/endpoint', 'results', true);
+        returnedPromise.then(() => {
+            expect(myState.results).toEqual('set');
+
+            fetch.mockClear();
+            informThatTestIsDone();
+        });
     });
 
-    let myState = { results: 'unchanged', apiFetchCompleted: false };
-    let objThis = {
-        setState: (newState) => {
-            myState = newState;
-        }
-    };
-    expect(myState.results).toEqual('unchanged');
+    // it('throws an error when fetch() fails'); TODO
+});
 
-    const promiseResult = fetchFromApi.call(objThis, 'api/endpoint', 'results', true);
-
-    promiseResult.then(() => {
-        expect(myState.results).toEqual('set');
-
-        fetch.mockClear();
-        informJestThatTestIsDone();
+describe('generateApiLoadingOrElements', () => {
+    it('emits a loading DOM element while fetch is in progress', () => {
+        let obj = {
+            state: {
+                apiFetchCompleted: false
+            }
+        };
+        let domObj = generateApiLoadingOrElements.call(obj);
+        let wrapper = shallow(domObj);
+        expect(wrapper.contains(<div>Loading...</div>)).toBeTruthy();
+        wrapper.unmount();
     });
-});
 
-// it('throws an error when fetch() fails'); TODO
+    it('emits an error DOM element with the specified after a fetch fails', () => {
+        let obj = {
+            state: {
+                apiFetchCompleted: true,
+                results: null,
+                apiFetchFailureMessage: 'Fetch failed!'
+            }
+        };
+        let domObj = generateApiLoadingOrElements.call(obj, 'results');
+        let wrapper = shallow(domObj);
+        expect(wrapper.text()).toContain('Fetch failed!');
+        wrapper.unmount();
+    });
 
-it('emits a loading DOM element while fetch is in progress', () => {
-    let obj = {
-        state: {
-            apiFetchCompleted: false
+    it('emits the return value of the generator function when a fetch was successful', () => {
+        let obj = {
+            state: {
+                apiFetchCompleted: true,
+                results: 'Contents!'
+            }
+        };
+
+        function domGenerator() {
+            return (
+                <div className='myClass'>{this.state.results}</div>
+            );
         }
-    };
-    let domObj = generateApiLoadingOrElements.call(obj);
-    let wrapper = shallow(domObj);
-    expect(wrapper.contains(<div>Loading...</div>)).toBeTruthy();
-    wrapper.unmount();
-});
 
-it('emits an error DOM element with the specified after a fetch fails', () => {
-    let obj = {
-        state: {
-            apiFetchCompleted: true,
-            results: null,
-            apiFetchFailureMessage: 'Fetch failed!'
-        }
-    };
-    let domObj = generateApiLoadingOrElements.call(obj, 'results');
-    let wrapper = shallow(domObj);
-    expect(wrapper.text()).toContain('Fetch failed!');
-    wrapper.unmount();
-});
-
-it('emits the return value of the generator function when a fetch was successful', () => {
-    let obj = {
-        state: {
-            apiFetchCompleted: true,
-            results: 'Contents!'
-        }
-    };
-    function domGenerator() {
-        return (
-            <div className='myClass'>{this.state.results}</div>
-        );
-    }
-    let domObj = generateApiLoadingOrElements.call(obj, 'results', domGenerator);
-    let wrapper = shallow(domObj);
-    expect(wrapper.find('.myClass').length).toBe(1);
-    expect(wrapper.text()).toContain('Contents!');
-    wrapper.unmount();
+        let domObj = generateApiLoadingOrElements.call(obj, 'results', domGenerator);
+        let wrapper = shallow(domObj);
+        expect(wrapper.find('.myClass').length).toBe(1);
+        expect(wrapper.text()).toContain('Contents!');
+        wrapper.unmount();
+    });
 });
